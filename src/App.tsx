48547815811dc6f2d84434ba12a9cba3725dc2d5
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-04-19 11:15:01 +0000

    Ajouter un state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react"
 
+// useState → stocker des données dynamiques
 
 function App() {
-    const pokemons: string[] = ["pikachu", "bulbasaur", "charmander"]
-
+    const data: string[] = ["pikachu", "bulbasaur", "charmander"]
+    const [pokemons, setPokemons] = useState<string[]>(data)
     return (
         <>
+            <p className="max-xl mx-auto px-4 mt-4 font-semibold">Nombre: {pokemons.length}</p>
             <div className="max-w-xl mx-auto px-4 space-y-4 mt-32  ">
                 {pokemons.map((poke) => (
                     <p className="shadow bg-gray-100 py-8 text-center rounded" key={poke}>{poke}</p>
