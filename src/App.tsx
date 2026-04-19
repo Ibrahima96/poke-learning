485ac7f4e405ba94c56750172df303e3a27c8b74
Ibrahima96 <ibrahimadiagne0811@gmail.com>
2026-04-19 10:55:58 +0000

    comprendre react

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
     return (
         <>
             <h1 className="text-3xl font-bold underline text-center cursor-pointer">
-                Hello world!
+              Mon Pokemon
             </h1>
         </>
     )
